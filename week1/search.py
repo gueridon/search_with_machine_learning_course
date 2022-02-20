17ac900b2fe30357b0c5983cf54a5b6d2ab681e7
gueridon <nicholasbacuez@gmail.com>
2022-02-20 13:37:44 +0000

    week 1 work

--- week1/search.py
+++ week1/search.py
@@ -15,12 +15,11 @@ INDEX_NAME = "bbuy_products"
 # display_filters -- return an array of filters that are applied that is appropriate for display
 # applied_filters -- return a String that is appropriate for inclusion in a URL as part of a query string.  This is basically the same as the input query string
 def process_filters(filters_input):
-    # Filters look like: &filter.name=regularPrice&regularPrice.key={{ agg.key }}&regularPrice.from={{ agg.from }}&regularPrice.to={{ agg.to }}
     filters = []
     display_filters = []  # Also create the text we will use to display the filters that are applied
     applied_filters = ""
     for filter in filters_input:
-        print(filter)
+        print(f"Filter pre-process: {filter}")
         type = request.args.get(filter + ".type")
         filter_to = request.args.get(filter + ".to")
         filter_from = request.args.get(filter + ".from")
@@ -30,12 +29,17 @@ def process_filters(filters_input):
         
         # We need to capture and return what filters are already applied so they can be automatically added to any existing links we display in aggregations.jinja2
         # TODO: IMPLEMENT AND SET filters, display_filters and applied_filters.
+        
         # filters get used in create_query below.  display_filters gets used by display_filters.jinja2 and applied_filters gets used by aggregations.jinja2 (and any other links that would execute a search.)
+        
+        # Filters look like: &filter.name=regularPrice&regularPrice.key={{ agg.key }}&regularPrice.from={{ agg.from }}&regularPrice.to={{ agg.to }}
+        
         ## FILTER TYPE == RANGE ########################################################
         if type == "range":
-            display_filters.append("filter.name={}, type={}, from={}, to={}".format(filter, type, filter_from, filter_to))
-            applied_filters += "&filter.name={}&{}.type={}&{}.from={}&{}.to={}&{}.key={}&{}.displayName={}".format(
-                filter, filter, type, filter, filter_from, filter, filter_to, filter, filter_key, filter, display_name)
+            display_filters.append(f"filter.name={filter}, type={type}, from={filter_from}, to={filter_to}")
+            applied_filters += f"&filter.name={filter}&{filter}.type={type}&{filter}.from={filter_from}" \
+                                f"&{filter}.to={filter_to}&{filter}.key={filter_key}&{filter}.displayName={display_name}"
+
             range_filter = ""
             if filter_from and filter_to:
                 range_filter = {
@@ -55,20 +59,15 @@ def process_filters(filters_input):
                     }
                 }
             filters.append(range_filter)
+
         ## FILTER TYPE == TERMS ########################################################
         elif type == "terms":
-            display_filters.append("filter.name={}, type={}, key={}".format(filter, type, filter_key))
-            applied_filters += "&filter.name={}&{}.type={}&{}.key={}&{}.displayName={}".format(
-                filter, filter, type, filter, filter_key, filter, display_name)
-            terms_filter = {
-                "term": {
-                    filter + ".keyword": filter_key
-                }
-            }
+            display_filters.append(f"filter.name={filter}, type={type}, key={filter_key}")
+            applied_filters += f"&filter.name={filter}&{filter}.type={type}&{filter}.key={filter_key}&{filter}.displayName={display_name}"
+            terms_filter = {"term": {filter + ".keyword": filter_key} }
             filters.append(terms_filter)
 
-    print("Filters: {}".format(filters))
-
+    print("Filters post-process: {}".format(filters))
     return filters, display_filters, applied_filters
 
 
@@ -127,14 +126,15 @@ def query():
 
 def create_query(user_query, filters, sort="_score", sortDir="desc"):
     print("Query: {} Filters: {} Sort: {}".format(user_query, filters, sort))
-    match_query_obj = {
+    base_query = {
         "multi_match": {
             "query": user_query,
             "fields": ["name^1000", "shortDescription^50", "longDescription^10", "department"]
         }
     }
+
     if user_query == '*':
-        match_query_obj = {
+        base_query = {
             "match_all": {}
         }
 
@@ -145,7 +145,7 @@ def create_query(user_query, filters, sort="_score", sortDir="desc"):
             "function_score": {
                 "query": {
                     "bool": {
-                        "must": [match_query_obj],
+                        "must": [base_query],
                         "filter": filters
                     }
                 },
@@ -159,7 +159,7 @@ def create_query(user_query, filters, sort="_score", sortDir="desc"):
                             "missing": 100000000,
                             "modifier": "reciprocal"
                         }
-                    }
+                    },
                     {
                         "field_value_factor": {
                             "field": "salesRankMediumTerm",
@@ -200,9 +200,15 @@ def create_query(user_query, filters, sort="_score", sortDir="desc"):
                     ]
                 }
             },
-            # TODO: add missing image
-        }
+            "missing_images": {
+                "missing": {
+                    "field": "image.keyword"
+                }
+            }
+        },
+        "_source": ["productId", "name", "shortDescription", "longDescription", "department", "salesRankShortTerm",  "salesRankMediumTerm", "salesRankLongTerm", "regularPrice"]
     }
+
     return query_obj
 
 
